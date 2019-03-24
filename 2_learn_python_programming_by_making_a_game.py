7d8c6695f6bac1ea022247cfa57f613dc347a4c6
Melanie <42304542+andoeme@users.noreply.github.com>
2019-03-24 09:11:18 +0100

    Add files via upload

--- 2_learn_python_programming_by_making_a_game.py
+++ 2_learn_python_programming_by_making_a_game.py
@@ -1,10 +1,38 @@
-# Get input from user via console
-inputName = input('Enter your name: ')
+# Python Game built with Pygame
 
-# Get type of variable
-print(type(5))
-print(type('five'))
+import pygame
 
-# Loops in Python
-for i in 'five':
-    print(i)
+pygame.init() # Initialize pgame
+
+# Set Up Game Screen
+screenWidth = 500
+screenHeight = 500
+screenTitle = 'Crossy Game'
+colorWhite = (255,255,255)
+colorBlack = (0,0,0)
+pygameClock = pygame.time.Clock()
+tickRate = 60 # FPS, frames per second
+isGameOver = False
+
+# Make Game Screen
+gameScreen = pygame.display.set_mode((screenWidth, screenHeight))
+gameScreen.fill(colorWhite)
+pygame.display.set_caption(screenTitle)
+
+# Game Loop
+# Stop game if isGameOver is True
+while not isGameOver:
+
+    # Handles and notices events in pygame
+    for event in pygame.event.get():
+        # Stop game if event is QUIT
+        if event.type == pygame.QUIT:
+            isGameOver = True
+        print(event)
+    
+    pygame.display.update() # Update screen
+    pygameClock.tick(tickRate) # Initializes the clock
+
+# Close game
+pygame.quit()
+quit()
